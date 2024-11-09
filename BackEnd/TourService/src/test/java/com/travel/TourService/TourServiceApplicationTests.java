1c191ab34b29cf87c96c2571b2e12282f3d4efb6
Hariram <bhariram01@gmail.com>
2024-11-09 18:32:08 +0530

    Initial Travel-Website project setup

---
+++ BackEnd/TourService/src/test/java/com/travel/TourService/TourServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.TourService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
